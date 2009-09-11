10b41f1bd74bec7108074d65bc51773a217a8f4a
David Cramer <dcramer@david-cramers-macbook.local>
2009-09-10 19:43:02 -0500

    Initial urls for samples

--- djangospot/apps/urls.py
+++ djangospot/apps/urls.py
@@ -3,6 +3,6 @@ from coffin.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
-    url(r'^', IndexController(), name='apps.index'),
-    url(r'submit^', SubmitController(), name='apps.submit'),
+    url(r'^', views.IndexController(), name='apps.index'),
+    url(r'submit^', views.SubmitController(), name='apps.submit'),
 )
